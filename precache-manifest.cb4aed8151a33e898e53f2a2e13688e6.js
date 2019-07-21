5d9b5a153ef886dc1d74f8dfe0d8dd36bdc0c5de
Serdar Mustafa <serdarmustafa@SerdarPsonalMBP.lan>
2019-07-21 13:11:14 +0300

    Updates

---
+++ precache-manifest.cb4aed8151a33e898e53f2a2e13688e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c60f689639bb85a16458e98f6aa39f5",
+    "url": "/React-lotto-numbers/index.html"
+  },
+  {
+    "revision": "c3240d982c3f4ebd85d7",
+    "url": "/React-lotto-numbers/static/css/main.9ef4b506.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/React-lotto-numbers/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "c3240d982c3f4ebd85d7",
+    "url": "/React-lotto-numbers/static/js/main.f74742c1.chunk.js"
+  },
+  {
+    "revision": "ad9a0de4a8fefabb0b8c",
+    "url": "/React-lotto-numbers/static/js/runtime~main.38496691.js"
+  }
+]);
